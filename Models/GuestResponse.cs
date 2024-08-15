9820283d05bc8b081b1ee2e8672142dba6d588dd
DELL <DELL@AYOBAMI>
2024-08-15 02:55:51 -0700

    Add project files.

---
+++ Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required (ErrorMessage = "Please enter your firstname")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your lastname")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your card number")]
+        public string? CardNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string? Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
